d18180742fa31c5453e95543828fd787dc1bf44f
frederickalcantara <frederickaalcantara@gmail.com>
2021-11-09 08:28:24 -0500

    Added files

---
+++ conditional.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main() {
+    int arr[] = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+    int factorial = 1;
+    int i;
+    for (i = 1; i <= 10; i++) 
+    {   
+        factorial *= i;
+    }
+    printf("10! is %d.\n", factorial);
+}
